cc99929f92c1276e1609019f5b6e6253a1fa175a
aurelien galoppe <agaloppe@me.com>
2017-05-07 13:27:11 +0200

    statics maps

--- app/helpers/quotations_helper.rb
+++ app/helpers/quotations_helper.rb
@@ -1,6 +1,6 @@
 module QuotationsHelper
   def full_address(city, zipcode, address)
-    @address = "#{address}, #{zipcode}, #{city.capitalize}"
+    @address = "#{address} #{zipcode} #{city.capitalize}"
   end
 
   def fullname(first, last)
